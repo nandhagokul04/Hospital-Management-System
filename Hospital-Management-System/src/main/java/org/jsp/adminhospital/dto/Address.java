3d1e6275781262e294ace0e3fa5dec5a03f01cf8
Suvo21 <mondalmai121@gmail.com>
2024-03-01 11:50:52 +0530

    packegeswere added

---
+++ Hospital-Management-System/src/main/java/org/jsp/adminhospital/dto/Address.java
@@ -0,0 +1,73 @@
+package org.jsp.adminhospital.dto;
+
+public class Address {
+
+	private int id;
+	private String street, city, landmark, building_name, state, country;
+	private int pincode;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getLandmark() {
+		return landmark;
+	}
+
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+
+	public String getBuilding_name() {
+		return building_name;
+	}
+
+	public void setBuilding_name(String building_name) {
+		this.building_name = building_name;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+}
---
+++ Hospital-Management-System/src/main/java/org/jsp/adminhospital/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package org.jsp.adminhospital.repository;
+
+import org.jsp.adminhospital.dto.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Integer>{
+
+}
