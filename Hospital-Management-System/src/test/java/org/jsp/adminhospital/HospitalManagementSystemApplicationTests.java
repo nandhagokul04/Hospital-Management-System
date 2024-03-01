6c3c6817f0b7b786b8788325c81519b4cb39ab7f
Suvo21 <mondalmai121@gmail.com>
2024-03-01 11:28:37 +0530

    first commit

---
+++ Hospital-Management-System/src/test/java/org/jsp/adminhospital/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.adminhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
